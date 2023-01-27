060a58b28fd9a423cd519a1eb1e7d292fe3d0240
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-01-27 22:49:14 +0300

    the section about me has been created and the section what are you interested in has been started?

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -20,4 +20,27 @@ document.addEventListener("DOMContentLoaded", () => {
       prevEl: ".btn__left__slider__prewie",
     },
   });
+  //conteiner-slider-interest
+   const swiper1 = new Swiper(".conteiner-slider-interest", {
+     // Optional parameters
+
+     loop: true,
+     slidesPerView: 2,
+    
+
+     /* breakpoints: {
+       1560: {
+         slidesPerView: 2,
+       },
+       768: {
+         slidesPerView: 1,
+       },
+     },*/
+
+     // Navigation arrows
+     navigation: {
+       nextEl: ".btn__right__slider__interest",
+       prevEl: ".btn__left__slider__interest",
+     },
+   });
 });

69684ff3da8d9179c65d4dd5e396d1a603757594
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-02-02 09:32:37 +0300

    I started to make up the blog 'about the unconscious', connected the script and styles for adaptations (I will refine)

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuLink = document.querySelectorAll(".menu-list__link");
+  const btnMobMenu = document.querySelector(".btn__mob-menu");
+  const btnMenuM = document.querySelector(".btn__m-menu");
+  const menuList = document.querySelector(".links");
+  const cover = document.querySelector(".menuCover");
+
+  const toggleIcon = () => {
+    if (btnMenuM.classList.contains("btn__m-menu")) {
+      btnMenuM.classList.remove("btn__m-menu");
+      btnMenuM.classList.add("btn__m-menu-active");
+      menuList.classList.add("is-open__mob-menu");
+      cover.classList.add("cover");
+    } else {
+      menuList.classList.remove("is-open__mob-menu");
+      btnMenuM.classList.remove("btn__m-menu-active");
+      cover.classList.remove("cover");
+      btnMenuM.classList.add("btn__m-menu");
+    }
+  };
+  btnMobMenu.addEventListener("click", toggleIcon);
+  cover.addEventListener("click", (event) => {
+    if (event.target == btnMenuM || event.target == cover) {
+      toggleIcon();
+    }
+  });
+
   menuLink.forEach((el, index) => {
     el.addEventListener("click", (event) => {
       const target = event.target;

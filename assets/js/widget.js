d78b5c5dc50e4055f8b8ec76a51cf0773adb1637
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-02-10 16:21:15 +0300

    modified script

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -4,28 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const itemWidget = document.querySelectorAll(".category-item");
   const coverWidget = document.querySelector(".cover-widget");
   console.log(window.screen.height);
- btnMoreWidget.addEventListener("click", () => {
+  btnMoreWidget.addEventListener("click", () => {
     itemWidget.forEach((item) => {
-      if (window.screen.width < 1000) {
-        if (
-          item.hasAttribute("data-role", "hidden") &&
-          !item.classList.contains("active_item_widget")
-        ) {
-          coverWidget.classList.add("active_category_widget");
-          item.classList.add("active_item_widget");
-          coverWidget.append(item);
-        } else {
-          coverWidget.classList.remove("active_category_widget");
-          item.classList.remove("active_item_widget");
-          categoryList.append(item);
-        }
-      } else {
+      if (
+        item.hasAttribute("data-role", "hidden") &&
+        !item.classList.contains("active_item_widget")
+      ) {
+        coverWidget.classList.add("active_category_widget");
+        item.classList.add("active_item_widget");
+        coverWidget.append(item);
+      } else if (
+        item.hasAttribute("data-role", "hidden") &&
+        item.classList.contains("active_item_widget")
+      ) {
         coverWidget.classList.remove("active_category_widget");
         item.classList.remove("active_item_widget");
         categoryList.append(item);
       }
-       
     });
   });
-    
 });
